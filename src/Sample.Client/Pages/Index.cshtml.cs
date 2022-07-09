d28bf9b65f81fb4ab8423c1bd1acdb6356ea061e
IndianBleach <84047808+IndianBleach@users.noreply.github.com>
2022-07-09 15:28:34 +0300

    Добавьте файлы проекта.

---
+++ src/Sample.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sample.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            HttpClientHandler h = new HttpClientHandler();
+            h.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+            {
+                return true;
+            };
+
+
+            using (var client = new HttpClient(h))
+            {
+
+                try
+                {
+                    var request = new HttpRequestMessage();
+                    request.RequestUri = new Uri("https://sample.webapi:443/counter");
+
+                    var response = await client.SendAsync(request);
+
+                    var counter = await response.Content.ReadAsStringAsync();
+                    ViewData["message"] = $"counter actual value: {counter}";
+                }
+                catch (Exception exp)
+                {
+                    _logger.LogCritical(exp.Message);
+                    ViewData["message"] = exp.Message;
+                }
+            }
+        }
+    }
+}
---
+++ src/Sample.WebApi/Extensions/DistributedCasheExntensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Sample.WebApi.Extensions
+{
+    public static class DistributedCasheExtensions
+    {
+        public static async Task SetRecordAsync<T>(
+            this IDistributedCache cashe,
+            string recordId,
+            T recordValue,
+            TimeSpan? expireTime = null,
+            TimeSpan? unusedTime = null)
+        {
+            DistributedCacheEntryOptions options = new()
+            {
+                AbsoluteExpirationRelativeToNow = expireTime ?? TimeSpan.FromSeconds(30),
+                SlidingExpiration = unusedTime
+            };
+
+            var json = JsonSerializer.Serialize(recordValue);
+            await cashe.SetStringAsync(recordId, json, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(
+            this IDistributedCache cashe,
+            string recordId)
+        {
+            var json = await cashe.GetStringAsync(recordId);
+
+            if (json == null) return default(T);
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
